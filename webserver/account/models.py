cf62b8f43b275d5b438d5031d8cbcf6475b63568
sy4k1 <xi3f4i@gmail.com>
2025-02-10 00:39:49 +0000

    feat(webserver): add models

--- webserver/account/models.py
+++ webserver/account/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Account(models.Model):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100, db_index=True)
+    # sha512 with salt
+    password = models.CharField(max_length=500, db_index=True)
+    role = models.CharField(max_length=20)
+    bio = models.TextField()
+    avatar = models.CharField(max_length=1000)
+    wallpaper = models.CharField(max_length=1000)
+    create_datetime = models.DateTimeField(default=now, blank=True, editable=False)
+
+
+class AccountAccessToken(models.Model):
+    account_email = models.EmailField(db_index=True)
+    # uuid as user access token
+    access_token = models.CharField(db_index=True, max_length=100)
+    create_datetime = models.DateTimeField(default=now, blank=True, editable=False)
+
+
+class AccountPasscode(models.Model):
+    account_email = models.EmailField(db_index=True)
+    passcode = models.CharField(max_length=10, db_index=True)
+    create_datetime = models.DateTimeField(default=now, blank=True, editable=False)
+
+
+class Follower(models.Model):
+    follower_email = models.EmailField(db_index=True)
+    followed_email = models.EmailField(db_index=True)
+    read = models.BooleanField(default=False)
+    create_datetime = models.DateTimeField(default=now, blank=True, editable=False)
--- webserver/post/models.py
+++ webserver/post/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, db_index=True)
+    content = models.TextField()
+    images = models.CharField(max_length=2000)
+    poster_email = models.EmailField(db_index=True)
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    channel = models.CharField(max_length=100, db_index=True)
+    create_datetime = models.DateTimeField(default=now, blank=True, editable=False)
+
+
+class LikedPost(models.Model):
+    liked_account_email = models.EmailField(db_index=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    poster_email = models.EmailField(db_index=True)
+    read = models.BooleanField(default=False)
+    create_datetime = models.DateTimeField(default=now, blank=True, editable=False)
+
+
+class Comment(models.Model):
+    parent_comment_id = models.BigIntegerField(db_index=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    poster_email = models.EmailField(db_index=True)
+    commentator_email = models.EmailField(db_index=True)
+    comment = models.TextField()
+    read = models.BooleanField(default=False)
+    create_datetime = models.DateTimeField(default=now, blank=True, editable=False)
